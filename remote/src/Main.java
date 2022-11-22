98477c4b158408e6ff4831033335b3d89e26fcd9
Clemens Pohl <60157388+ClemensPohl@users.noreply.github.com>
2022-11-22 16:18:45 +0100

    Remote Finished

--- remote/src/Main.java
+++ remote/src/Main.java
@@ -10,6 +10,5 @@ public class Main {
         remote.TurnOn();
         remote.TurnOff();
         remote.getStatus();
-
     }
 }
--- remote/src/Remote.java
+++ remote/src/Remote.java
@@ -36,7 +36,4 @@ public class Remote {
         }
         System.out.println("Mittelwert ist: " + x / this.battery.size() + "%");
     }
-
-
-
 }
